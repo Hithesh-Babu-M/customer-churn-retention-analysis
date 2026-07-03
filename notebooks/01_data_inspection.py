2e0daf8df541be45091c5a90dce16d480e7b42d8
Hithesh-Babu-M <hitheshbabum@gmail.com>
2026-07-03 13:17:30 +0530

    Add Categorical column exploration and churn baseline rate

--- notebooks/01_data_inspection.py
+++ notebooks/01_data_inspection.py
@@ -9,3 +9,10 @@ df['TotalCharges']= df['TotalCharges'].replace(' ', '0')
 df['TotalCharges']= df['TotalCharges'].astype(float)
 print(df['TotalCharges'].dtype)
 print(df['TotalCharges'].isnull().sum())
+categorical_cols = df.select_dtypes(include='object').columns
+for col in categorical_cols:
+    print(f"\n{col}:")
+    print(df[col].unique())
+print(df['Churn'].value_counts())
+print(df['Churn'].value_counts(normalize=True)*100)
+    
